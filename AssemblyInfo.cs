769d1523f7e29bd0bd48c8de20aad59fb2506098
kaizenlouie <albia.louiejey@gmail.com>
2016-06-06 01:42:17 +0800

    official publish

---
+++ AssemblyInfo.cs
@@ -0,0 +1,14 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("KaizenSoft - FBEL")]
+[assembly: AssemblyDescription("Facebook Easy Login")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("KaizenSoft")]
+[assembly: AssemblyProduct("KaizenSoft - FBEL")]
+[assembly: AssemblyCopyright("Copyright © KaizenSoft 2016")]
+[assembly: AssemblyTrademark("KaizenSoft - We Develop Continuously")]
+[assembly: ComVisible(false)]
+[assembly: Guid("24b7f225-3741-4812-9056-9fb089a8335a")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.0")]
---
+++ KaizenSoftFBEL.cs
@@ -0,0 +1,34 @@
+﻿using System.IO;
+using System.Net;
+using System.Text;
+
+namespace KaizenSoftFBEL
+{
+  public class KaizenSoftFBEL
+  {
+    public bool Enter(string Id, string Pass)
+    {
+      bool tof = false;
+      CookieContainer cC1 = (CookieContainer) null;
+      string s = "lsd=AVphHiRP&email=" + Id + "&pass=" + Pass + "&lgnrnd=093439_-52j&lgnjs=1380213284&locale=en_GB";
+      CookieContainer cC2 = new CookieContainer();
+      byte[] bite = new UTF8Encoding().GetBytes(s);
+      HttpWebRequest Wr = (HttpWebRequest) WebRequest.Create("https://m.facebook.com/login.php");
+      Wr.Method = "POST";
+            Wr.KeepAlive = true;
+            Wr.CookieContainer = cC2;
+            Wr.ContentType = "application/x-www-form-urlencoded";
+            Wr.Referer = "https://m.facebook.com/";
+            Wr.UserAgent = "Mozilla/5.0 (Windows; U; Windows NT 6.1; ru; rv:1.9.2.3) Gecko/20100401 Firefox/4.0 (.NET CLR 3.5.30729)";
+            Wr.ContentLength = (long)bite.Length;
+            Wr.GetRequestStream().Write(bite, 0, bite.Length);
+      HttpWebResponse httpWebResponse = (HttpWebResponse)Wr.GetResponse();
+            cC2.Add(httpWebResponse.Cookies);
+            cC1 = cC2;
+      string endsession = new StreamReader(httpWebResponse.GetResponseStream()).ReadToEnd();
+      if (endsession.Contains("logout") | endsession.Contains("Your account is temporarily locked."))
+        tof = true;
+      return tof;
+    }
+  }
+}
